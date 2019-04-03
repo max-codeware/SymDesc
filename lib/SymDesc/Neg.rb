2d8425daeabb638e1e709730093ce67325311509
max-codeware <max.codeware@gmail.com>
2019-04-03 18:52:03 +0200

    Added some documentation

--- lib/SymDesc/Neg.rb
+++ lib/SymDesc/Neg.rb
@@ -10,6 +10,9 @@ module SymDesc
     # |_| \_|\___|\__, |
     #             |___/ 
 
+    # This class represents the unary negation of a symbolic
+    # object. It simply wraps a symbolic expression and manipulates
+    # it accurding to unary negation.
     class Neg
 
     	include Base
@@ -20,6 +23,10 @@ module SymDesc
     		alias   :__new :new 
     		private :__new
 
+            # Creates a new negated expression with automatic
+            # simplification if necessary.
+            # If the passed object is not a symbolic one,
+            # a conversion is attempted.
     		def new(arg)
     			arg = arg.symdescfy
     			if arg.is_a? Neg
@@ -33,7 +40,19 @@ module SymDesc
     		@argument = argument
     	end
 
-
+        # :call-seq:
+        #   neg + obj -> new_obj
+        #
+        # It sums `obj` to `neg` returning a new object
+        # as result of the operation.
+        #
+        # Automatic simplifications are:
+        # ```
+        # (-a) + b    -> b - a
+        # (-a) + (-b) -> -(a + b)
+        # ```
+        #
+        # If `obj` is not symbolic, a conversion is attempted 
     	def +(b)
     		return case b
     		    when Neg
@@ -53,6 +72,19 @@ module SymDesc
     		tmp 
     	end
 
+        # :call-seq:
+        #   neg  obj -> new_obj
+        #
+        # It subtracts `obj` to `neg` returning a new object
+        # as result of the operation.
+        #
+        # Automatic simplifications are:
+        # ```
+        # (-a) - b    -> -(a + b)
+        # (-a) - (-b) -> b - a
+        # ```
+        #
+        # If `obj` is not symbolic, a conversion is attempted 
     	def -(b)
     		return case b
     		    when Neg
@@ -72,16 +104,34 @@ module SymDesc
     		tmp 
     	end
 
+        # :call-seq:
+        #   -neg -> argument
+        #
+        # It performs the unary negation of `neg`.
+        # Since simplification is automatic it returns the
+        # argument of `neg`
     	def -@
     		argument 
     	end
 
+        # :call-seq:
+        #   `neg` == `obj`
+        #
+        # It compares `obj` with `neg`, returning true if `obj`
+        # is a Neg and has the same argument.
     	def ==(obj)
             obj = obj.symdescfy
     		return true if obj.is_a?(Neg) && obj.argument == argument
     		false
     	end
 
+        # :call-seq:
+        #   to_s -> string
+        #   to_s(str_io) -> str_io
+        #
+        # If no argument is provided, it returns a string representation
+        # of the unary negation. If a StringIO object is passed, the string
+        # representation is appended to the buffer and this last is returned.
     	def to_s(io = nil)
             _io = io || StringIO.new
             case argument
@@ -91,8 +141,8 @@ module SymDesc
                     __io_append(io,SUB_ID,@argument)
             end
             if !io
-            	io.close
-            	return io.string 
+            	_io.close
+            	return _io.string 
             end
             return io
     	end
