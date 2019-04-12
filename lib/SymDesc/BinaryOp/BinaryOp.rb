40cbd3f91d67d9a01a62fb9b5a5b809ea582280a
max-codeware <max.codeware@gmail.com>
2019-04-12 22:25:28 +0200

    Implemented part of BinaryOp class

--- lib/SymDesc/BinaryOp/BinaryOp.rb
+++ lib/SymDesc/BinaryOp/BinaryOp.rb
@@ -1,5 +1,28 @@
 module SymDesc
     class BinaryOp
     	include Base
+
+        attr_reader :left,:right
+
+    	def initialize(left,right)
+    		@left  = left
+    		@right = right
+    	end
+
+        def ==(b)
+        	(self.class = b.class) && 
+        	(((left == b.left) && (right == b.right))  ||
+        	((left  == b.right) && (right == b.left)))
+        end
+
+    protected 
+        def left=(b)
+        	@left = left
+        end
+
+        def right=(b)
+        	@right = right
+        end
     end
+
 end
