7502df71e41c698ee12ea66fc40e697746746817
max-codeware <max.codeware@gmail.com>
2019-04-28 20:36:50 +0200

    Code cleaning

--- lib/SymDesc/Number/Ratio.rb
+++ lib/SymDesc/Number/Ratio.rb
@@ -70,54 +70,6 @@ module SymDesc
                 end
                 false
             end
-    
-            def __ratio_from_numeric(n)
-                @@precision ||= (SYM_CONFIG[:ratio_precision] || 1e-16)
-                if n.is_a? Integer
-                	return n,1
-                end
-                if n.is_a? Rational 
-                	return n.numerator,n.denominator
-                end
-                n1, n2 = 1,0
-                d1, d2 = 0,1
-                v      = n
-                loop do
-                    v1  = v.floor
-                    tmp = n1
-                    n1  = v1 * n1 + n2 
-                    n2  = tmp
-
-                    tmp = d1
-                    d1  = v1 * d1 + d2 
-                    d2  = tmp
-                    v   = 1.0/(v - v1)
-                    break if v == Float::INFINITY
-                    break if (n - n1/d1.to_f).abs < n * @@precision
-                end
-                #t = n.round
-                #return t,1 if (d1*t - n1/d1.to_f).abs < n * @@precision
-                return n1,d1
-            end
-    
-            # Given two numbers `a` and `b`, and knowing
-            # at least one is a float (or a rational) computes
-            # the numerator and the denominator of the division
-            # `a/b`
-            def __ratio_from_numeric2(a,b)
-                n1,d1 = __ratio_from_numeric(a)
-                n2,d2 = __ratio_from_numeric(b)
-                return n1*d2, d1*n2
-            end
-    
-            def __mcd(a,b)
-            	return 1 if a.zero? || b.zero?
-                a,b = b,a unless b < a 
-                while b != 0 
-                    a,b = b, a % b 
-                end 
-                return a 
-            end
     	end # End metaclass
     
     	attr_reader :numerator
