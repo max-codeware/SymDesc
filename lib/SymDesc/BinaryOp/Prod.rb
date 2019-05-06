f7b2d9e51d7eb2879922464ce9097185a77d4f66
max-codeware <max.codeware@gmail.com>
2019-05-06 22:36:03 +0200

    Implemented part of Prod class

--- lib/SymDesc/BinaryOp/Prod.rb
+++ lib/SymDesc/BinaryOp/Prod.rb
@@ -1,6 +1,60 @@
 
 module SymDesc
+
     class Prod < BinaryOp
 
-    end	
+        class <<self
+            def new(left,right)
+                return ZERO  if left == 0 || right == 0
+                return left  if right == 1
+                return right if left == 1
+                return super
+            end
+        end
+
+        def +(b)
+            return TWO * b if self == b 
+            b = b.symdescfy
+            return case b
+                when Variable
+                	__sum_variable b 
+                when Prod,Div
+                	__sum_prod_div b 
+                else
+                	__sum_else b 
+            end
+        end
+
+        def opt_sum(b) #:nodoc:
+        end
+
+        def -(b)
+        end
+
+        def opt_sub(b) # :nodoc:
+        end
+
+        def to_s(io = nil)
+        end
+
+        def =~(b) # :nodoc:
+        	return (@left.is_a? Number) && (@right == b)
+        end
+
+    private
+
+        def __sum_variable(b)
+            return self =~ b ? (Prod.new(@left + 1, @right)) : Sum.new(self,b)
+        end
+
+        def __sum_prod_div(b)
+        end
+
+        def __sum_else(b)
+            return self =~ b ? (Prod.new(@left + 1, @right)) : Sum.new(self,b)
+        end
+
+    end
+
+
 end
