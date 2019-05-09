bd51168410a99543ea2a11725ece001ec9f3feb8
max-codeware <max.codeware@gmail.com>
2019-05-09 13:34:16 +0200

    Added Kernel#cas method

--- lib/SymDesc/Overload/Kernel.rb
+++ lib/SymDesc/Overload/Kernel.rb
@@ -7,4 +7,9 @@ module Kernel
         end
         return (args.size == 1) ? args[0] : args
     end
+
+    def cas(*args)
+    	args.map! { |v| v.is_a?(Symbol) ? var(v) : v.symdescfy }
+    	return (args.size == 1) ? args[0] : args
+    end
 end
