f4a2665e35091d974da226112e6b6a2e31e85610
max-codeware <max.codeware@gmail.com>
2019-05-19 16:59:53 +0200

    Added license header & copyright

--- lib/SymDesc/Number/Int.rb
+++ lib/SymDesc/Number/Int.rb
@@ -1,3 +1,24 @@
+# MIT License
+# 
+# Copyright (c) 2019 Massimiliano Dal Mas
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 module SymDesc
     #  ___       _   
@@ -6,12 +27,14 @@ module SymDesc
     #  | || | | | |_ 
     # |___|_| |_|\__|
 
+    ##
     # This class represents and wraps an integer value
     # as a symbolic int.
     class Int < Number
     
         class <<self
     
+            ##
             # :call-seq:
             #   new(i) -> new_int
             #
