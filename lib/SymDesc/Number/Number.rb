618ed510d1f129fc942c2a953a23938eac5b714b
max-codeware <max.codeware@gmail.com>
2019-08-30 22:22:21 +0200

    Unified code for Int and Ratio

--- lib/SymDesc/Number/Number.rb
+++ lib/SymDesc/Number/Number.rb
@@ -25,6 +25,77 @@ module SymDesc
     REQUIRES_BASIC_OP = true
     include Base
 
+    ##
+    # Returns SymDesc::Number negated (wrapped in SymDesc::Neg class)
+    # This method is used by numeric subclasses
+    def -@
+      return Neg.new self
+    end
+
+    ##
+    # :call-seq:
+    #   number * obj -> new_obj
+    #
+    # Performs a symbolic multiplication between a symbolic rational
+    # and `obj`, returning a new symbolic object if the operation
+    # creates a new tree branch, or a symbolic number if
+    # `obj` is a SymDesc::Number. Simplification is automatic.
+    #
+    # This method is used by SymDesc::Ratio ans SymDesc::Int
+    # If b is not a symbolic object, a conversion is attempted
+    def *(b)
+      b = b.symdescfy
+      case b
+      when Number
+        __prod_number(b)
+      when Infinity
+        b
+      else
+        super
+      end
+    end
+
+    def opt_prod(b) # :nodoc:
+      case b
+      when Number, Infinity
+        self * b
+      else
+        super
+      end
+    end
+
+    ##
+    # :call-seq:
+    #   number / obj -> new_obj
+    #
+    # Performs a symbolic division between a symbolic number
+    # and `obj`, returning a new symbolic object if the operation
+    # creates a new tree branch, or a symbolic number if
+    # `obj` is a SymDesc::Number. Simplification is automatic.
+    #
+    # This method is used by SymDesc::Ratio ans SymDesc::Int
+    # If b is not a symbolic object, a conversion is attempted
+    def /(b)
+      b = b.symdescfy
+      case b
+      when Number
+        __div_number(b)
+      when Infinity
+        ZERO
+      else
+        super
+      end
+    end
+
+    def opt_div(b) # :nodoc:
+      case b
+      when Number, Infinity
+        self / b
+      else
+        super
+      end
+    end
+
     def diff(*v)
       __diff(v) { ZERO }
     end
@@ -35,6 +106,38 @@ module SymDesc
 
     def inspect(io = nil)
       to_s io
+    end
+
+    protected
+
+    def __prod_number(b)
+      case
+      when self.is_a?(Ratio) && b.is_a?(Ratio)
+        Ratio.new(@numerator * b.numerator, @denominator * b.denominator)
+      when self.is_a?(Int) && b.is_a?(Int)
+        Int.new(@value * b.value)
+      when self.is_a?(Int) && b.is_a?(Ratio)
+        Ratio.new(@value * b.numerator, b.denominator)
+      when self.is_a?(Ratio) && b.is_a?(Int)
+        Ratio.new(b.value * @numerator, @denominator)
+      else
+        raise Bug, "Received unhandled Number type (#{b.inspect})"
+      end
+    end
+
+    def __div_number(b)
+      case
+      when self.is_a?(Ratio) && b.is_a?(Ratio)
+        Ratio.new(@numerator * b.denominator, @denominator * b.numerator)
+      when self.is_a?(Int) && b.is_a?(Int)
+        Ratio.new(@value, b.value)
+      when self.is_a?(Int) && b.is_a?(Ratio)
+        Ratio.new(@value * b.denominator, b.numerator)
+      when self.is_a?(Ratio) && b.is_a?(Int)
+        Ratio.new(@numerator, @denominator * b.value)
+      else
+        raise Bug, "Received unhandled Number type (#{b.inspect})"
+      end
     end
   end
 end
